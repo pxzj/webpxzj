ee158861174b9ab6c3a5968dc9b3be9caee6a2ec
pengxin <lovexx07@outlook.com>
2020-05-01 14:59:22 +0800

    网页初次提交

--- official_website/src/main/java/com/jq/controller/IndexController.java
+++ official_website/src/main/java/com/jq/controller/IndexController.java
@@ -1,14 +1,22 @@
 package com.jq.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
-@RestController
+@Controller
 public class IndexController {
-	
-	 @RequestMapping("/")
-	    public String index(){
-	        return "欢迎登陆网页";
-	    }
-	
+
+	@RequestMapping("/")
+	public String home() {
+		return "欢迎登陆网页";
+	}
+
+	@RequestMapping("/index")
+	public String index() {
+		ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("index");
+        modelAndView.addObject("company", "纪青空间设计");
+		return "index";
+	}
 }
