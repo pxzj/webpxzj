43ff6251d675dbbfe589afed9851eef88d999841
pengxin <lovexx07@outlook.com>
2020-05-02 13:59:18 +0800

    页面与后端交互案例

--- official_website/src/main/java/com/jq/controller/CaseController.java
+++ official_website/src/main/java/com/jq/controller/CaseController.java
@@ -1,9 +1,9 @@
 package com.jq.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,25 +14,44 @@ import com.jq.service.CaseService;
 @RestController
 public class CaseController {
 
- @Autowired	
-  CaseService caseService;	
-	
-     @RequestMapping("/selectAll")
-    public List<String> selectAll(){
-    	
-    	List<String>  mainCaseList = caseService.selectAll();
-    	
-    	return mainCaseList;
-     }	
-	
-     @RequestMapping("/selectSubCaseByMainCase")
- public List<WebCase> selectSubCaseByMainCase(@RequestParam("mainCase") String mainCase){
-    	
-    	 List<WebCase> subCaseList = caseService.selectSubCaseByMainCase(mainCase);
-    	 
-    	 return subCaseList;
-    	 
-     }	
-     
+    @Autowired
+    CaseService caseService;
+
+    @RequestMapping("/selectAll")
+    public List<String> selectAll() {
+
+        List<String> mainCaseList = caseService.selectAll();
+
+        return mainCaseList;
+    }
+
+    @RequestMapping("/selectSubCaseByMainCase")
+    public List<WebCase> selectSubCaseByMainCase(@RequestParam("mainCase") String mainCase) {
+
+        List<WebCase> subCaseList = caseService.selectSubCaseByMainCase(mainCase);
+
+        return subCaseList;
+
+    }
+    @RequestMapping("/allImgs")
+    public List<WebCase> allImgs(){
+        List<WebCase> mainCaseList = new ArrayList<>();
+        WebCase web = new WebCase();
+        web.setMainCasePhoto("http://localhost:8080/img/gallery-img-01-tn.jpg");
+        web.setHeight("300");
+        web.setWidth("400");
+        mainCaseList.add(web);
+
+        web.setHeight("400");
+        web.setWidth("530");
+
+        mainCaseList.add(web);
+
+        web.setHeight("300");
+        web.setWidth("500");
+
+        mainCaseList.add(web);
+        return mainCaseList;
+    }
 
 }
--- official_website/src/main/java/com/jq/pojo/WebCase.java
+++ official_website/src/main/java/com/jq/pojo/WebCase.java
@@ -4,130 +4,178 @@ import java.sql.Date;
 import java.sql.Timestamp;
 
 public class WebCase {
-	
-	 private Long Id;// 自增主键ID
-	 private String mainCasePhoto;// 主案例图片URL
-	 private String subCasePhoto;// 子案例图片URL
-	 private String projectName;// 项目名称
-	 private int projectArea;// 项目面积
-	 private Date endDate;// 项目结束时间
-	 private String projectAddress;// 项目地址
-	 private String designStyle;// 设计风格
-	 private String mainMaterial;// 主要材料
-	 private String designCompany;// 设计单位
-	 private String designer;// 设计师
-	 private String assistDisigner;// 辅助设计师
-	 private String photographer;// 摄影师
-	 private String sales;// 营销顾问
-	 private Timestamp created;// 创建时间
-	 private Timestamp updated;// 更新时间
-	public Long getId() {
-		return Id;
-	}
-	public void setId(Long id) {
-		Id = id;
-	}
-	public String getMainCasePhoto() {
-		return mainCasePhoto;
-	}
-	public void setMainCasePhoto(String mainCasePhoto) {
-		this.mainCasePhoto = mainCasePhoto;
-	}
-	public String getSubCasePhoto() {
-		return subCasePhoto;
-	}
-	public void setSubCasePhoto(String subCasePhoto) {
-		this.subCasePhoto = subCasePhoto;
-	}
-	public String getProjectName() {
-		return projectName;
-	}
-	public void setProjectName(String projectName) {
-		this.projectName = projectName;
-	}
-	public int getProjectArea() {
-		return projectArea;
-	}
-	public void setProjectArea(int projectArea) {
-		this.projectArea = projectArea;
-	}
-	public Date getEndDate() {
-		return endDate;
-	}
-	public void setEndDate(Date endDate) {
-		this.endDate = endDate;
-	}
-	public String getProjectAddress() {
-		return projectAddress;
-	}
-	public void setProjectAddress(String projectAddress) {
-		this.projectAddress = projectAddress;
-	}
-	public String getDesignStyle() {
-		return designStyle;
-	}
-	public void setDesignStyle(String designStyle) {
-		this.designStyle = designStyle;
-	}
-	public String getMainMaterial() {
-		return mainMaterial;
-	}
-	public void setMainMaterial(String mainMaterial) {
-		this.mainMaterial = mainMaterial;
-	}
-	public String getDesignCompany() {
-		return designCompany;
-	}
-	public void setDesignCompany(String designCompany) {
-		this.designCompany = designCompany;
-	}
-	public String getDesigner() {
-		return designer;
-	}
-	public void setDesigner(String designer) {
-		this.designer = designer;
-	}
-	public String getAssistDisigner() {
-		return assistDisigner;
-	}
-	public void setAssistDisigner(String assistDisigner) {
-		this.assistDisigner = assistDisigner;
-	}
-	public String getPhotographer() {
-		return photographer;
-	}
-	public void setPhotographer(String photographer) {
-		this.photographer = photographer;
-	}
-	public String getSales() {
-		return sales;
-	}
-	public void setSales(String sales) {
-		this.sales = sales;
-	}
-	public Timestamp getCreated() {
-		return created;
-	}
-	public void setCreated(Timestamp created) {
-		this.created = created;
-	}
-	public Timestamp getUpdated() {
-		return updated;
-	}
-	public void setUpdated(Timestamp updated) {
-		this.updated = updated;
-	}
-	@Override
-	public String toString() {
-		return "WebCase [Id=" + Id + ", mainCasePhoto=" + mainCasePhoto + ", subCasePhoto=" + subCasePhoto
-				+ ", projectName=" + projectName + ", projectArea=" + projectArea + ", endDate=" + endDate
-				+ ", projectAddress=" + projectAddress + ", designStyle=" + designStyle + ", mainMaterial="
-				+ mainMaterial + ", designCompany=" + designCompany + ", designer=" + designer + ", assistDisigner="
-				+ assistDisigner + ", photographer=" + photographer + ", sales=" + sales + ", created=" + created
-				+ ", updated=" + updated + "]";
-	}
-	 
-	 
-	 
-	
+
+    private Long Id;// 自增主键ID
+    private String mainCasePhoto;// 主案例图片URL
+    private String subCasePhoto;// 子案例图片URL
+    private String projectName;// 项目名称
+    private int projectArea;// 项目面积
+    private Date endDate;// 项目结束时间
+    private String projectAddress;// 项目地址
+    private String designStyle;// 设计风格
+    private String mainMaterial;// 主要材料
+    private String designCompany;// 设计单位
+    private String designer;// 设计师
+    private String assistDisigner;// 辅助设计师
+    private String photographer;// 摄影师
+    private String sales;// 营销顾问
+    private Timestamp created;// 创建时间
+    private Timestamp updated;// 更新时间
+    private String width;
+    private String height;
+
+    public String getWidth() {
+        return this.width;
+    }
+
+    public String getHeight() {
+        return this.height;
+    }
+
+    public void setWidth(String width) {
+        this.width = width;
+    }
+
+    public void setHeight(String height) {
+        this.height = height;
+    }
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public String getMainCasePhoto() {
+        return mainCasePhoto;
+    }
+
+    public void setMainCasePhoto(String mainCasePhoto) {
+        this.mainCasePhoto = mainCasePhoto;
+    }
+
+    public String getSubCasePhoto() {
+        return subCasePhoto;
+    }
+
+    public void setSubCasePhoto(String subCasePhoto) {
+        this.subCasePhoto = subCasePhoto;
+    }
+
+    public String getProjectName() {
+        return projectName;
+    }
+
+    public void setProjectName(String projectName) {
+        this.projectName = projectName;
+    }
+
+    public int getProjectArea() {
+        return projectArea;
+    }
+
+    public void setProjectArea(int projectArea) {
+        this.projectArea = projectArea;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getProjectAddress() {
+        return projectAddress;
+    }
+
+    public void setProjectAddress(String projectAddress) {
+        this.projectAddress = projectAddress;
+    }
+
+    public String getDesignStyle() {
+        return designStyle;
+    }
+
+    public void setDesignStyle(String designStyle) {
+        this.designStyle = designStyle;
+    }
+
+    public String getMainMaterial() {
+        return mainMaterial;
+    }
+
+    public void setMainMaterial(String mainMaterial) {
+        this.mainMaterial = mainMaterial;
+    }
+
+    public String getDesignCompany() {
+        return designCompany;
+    }
+
+    public void setDesignCompany(String designCompany) {
+        this.designCompany = designCompany;
+    }
+
+    public String getDesigner() {
+        return designer;
+    }
+
+    public void setDesigner(String designer) {
+        this.designer = designer;
+    }
+
+    public String getAssistDisigner() {
+        return assistDisigner;
+    }
+
+    public void setAssistDisigner(String assistDisigner) {
+        this.assistDisigner = assistDisigner;
+    }
+
+    public String getPhotographer() {
+        return photographer;
+    }
+
+    public void setPhotographer(String photographer) {
+        this.photographer = photographer;
+    }
+
+    public String getSales() {
+        return sales;
+    }
+
+    public void setSales(String sales) {
+        this.sales = sales;
+    }
+
+    public Timestamp getCreated() {
+        return created;
+    }
+
+    public void setCreated(Timestamp created) {
+        this.created = created;
+    }
+
+    public Timestamp getUpdated() {
+        return updated;
+    }
+
+    public void setUpdated(Timestamp updated) {
+        this.updated = updated;
+    }
+
+    @Override
+    public String toString() {
+        return "WebCase [Id=" + Id + ", mainCasePhoto=" + mainCasePhoto + ", subCasePhoto=" + subCasePhoto
+                + ", projectName=" + projectName + ", projectArea=" + projectArea + ", endDate=" + endDate
+                + ", projectAddress=" + projectAddress + ", designStyle=" + designStyle + ", mainMaterial="
+                + mainMaterial + ", designCompany=" + designCompany + ", designer=" + designer + ", assistDisigner="
+                + assistDisigner + ", photographer=" + photographer + ", sales=" + sales + ", created=" + created
+                + ", updated=" + updated + "]";
+    }
+
 }
