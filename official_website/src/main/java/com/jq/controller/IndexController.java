abb367328b5624379ac32b8c968c422235672517
pengxin <lovexx07@outlook.com>
2020-05-04 15:17:46 +0800

    修改登陆业，增加错误页

--- official_website/src/main/java/com/jq/controller/IndexController.java
+++ official_website/src/main/java/com/jq/controller/IndexController.java
@@ -1,35 +1,62 @@
 package com.jq.controller;
 
+import java.util.Map;
+
+import com.alibaba.druid.util.StringUtils;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class IndexController {
 
+    @RequestMapping("/index")
+    public String index() {
+        return "index";
+    }
 
-	@RequestMapping("/index")
-	public String index() {
-		return "index";
-    }
     @RequestMapping("/service")
-	public String service() {
-		return "service";
+    public String service() {
+        return "service";
     }
+
     @RequestMapping("/work")
-	public String work() {
-		return "work";
+    public String work() {
+        return "work";
     }
+
     @RequestMapping("/topic")
-	public String topic() {
-		return "topic";
+    public String topic() {
+        return "topic";
     }
+
     @RequestMapping("/recuit")
-	public String recuit() {
-		return "recuit";
+    public String recuit() {
+        return "recuit";
     }
+
     @RequestMapping("/contact")
-	public String contact() {
-		return "contact";
-	}
+    public String contact() {
+        return "contact";
+    }
+
+    @RequestMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @RequestMapping("/loginadmin")
+    public ModelAndView loginadmin(@RequestParam("username") String username, @RequestParam("password") String password,
+            Map<String, Object> map) {
+        ModelAndView success = new ModelAndView();
+        if ("admin".equals(username) && "admin".equals(password)) {
+            success.setViewName("manage");
+        } else{
+            success.setViewName("error");
+        }
+        return success;
+    }
 }
