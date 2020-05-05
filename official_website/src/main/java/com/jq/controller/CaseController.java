d06692994718012e3d7c41e3608f57b0acb70ac7
pengxin <lovexx07@outlook.com>
2020-05-05 16:51:49 +0800

    案例页面交互以及页面优化显示

--- official_website/src/main/java/com/jq/controller/CaseController.java
+++ official_website/src/main/java/com/jq/controller/CaseController.java
@@ -41,20 +41,39 @@ public class CaseController {
      public List<WebCase> allImgs(){
          List<WebCase> mainCaseList = new ArrayList<>();
          WebCase web = new WebCase();
-         web.setMainCasePhoto("http://localhost:8080/img/gallery-img-01-tn.jpg");
-         web.setHeight("300");
-         web.setWidth("400");
+         web.setMainCasePhoto("http://localhost:8080/images/work01.jpg");
+         web.setHeight("2744");
+         web.setWidth("2607");
+         web.setSnId(1L);
          mainCaseList.add(web);
 
-         web.setHeight("400");
-         web.setWidth("530");
+         web = new WebCase();
+         web.setMainCasePhoto("http://localhost:8080/images/work02.jpg");
+         web.setHeight("8688");
+         web.setWidth("5792");
+         web.setSnId(2L);
 
          mainCaseList.add(web);
-
+         web = new WebCase();
          web.setHeight("300");
          web.setWidth("500");
+         web.setSnId(3L);
 
          mainCaseList.add(web);
+         web = new WebCase();
+         web.setMainCasePhoto("http://localhost:8080/images/work01.jpg");
+         web.setHeight("2744");
+         web.setWidth("2607");
+         web.setSnId(4L);
+         mainCaseList.add(web);
+         web = new WebCase();
+         web.setMainCasePhoto("http://localhost:8080/images/work02.jpg");
+         web.setHeight("8688");
+         web.setWidth("5792");
+         web.setSnId(5L);
+         mainCaseList.add(web);
+
+
          return mainCaseList;
      }
   
--- official_website/src/main/java/com/jq/controller/IndexController.java
+++ official_website/src/main/java/com/jq/controller/IndexController.java
@@ -5,6 +5,7 @@ import java.util.Map;
 import com.alibaba.druid.util.StringUtils;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -43,4 +44,11 @@ public class IndexController {
         return "contact";
     }
 
+    @RequestMapping("/workdetail")
+    public ModelAndView workdetail(@RequestParam("snId") String snId) {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("id", snId);
+        modelAndView.setViewName("workdetail");
+        return modelAndView;
+    }
 }
